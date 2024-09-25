ed10620a51d304505e6f62548de1519b666aa440
mannumishra <mannumishra2207@outlook.com>
2024-09-25 16:24:30 +0530

    add chnges

--- Routes/itemServiceRoutes.js
+++ Routes/itemServiceRoutes.js
@@ -1,11 +1,11 @@
-const { createService } = require("../Controller/ItemServiceController")
+const { createService, getAllServices, getServiceById, updateService, deleteService } = require("../Controller/ItemServiceController")
 
 const itemServiceRoutes = require("express").Router()
 
 itemServiceRoutes.post("/create-service", createService)
-itemServiceRoutes.get("/get-all-service", createService)
-itemServiceRoutes.get("/get-single-service/:id", createService)
-itemServiceRoutes.put("/update-service/:id", createService)
-itemServiceRoutes.delete("/delete-servic/:id", createService)
+itemServiceRoutes.get("/get-all-service", getAllServices)
+itemServiceRoutes.get("/get-single-service/:id", getServiceById)
+itemServiceRoutes.put("/update-service/:id", updateService)
+itemServiceRoutes.delete("/delete-servic/:id", deleteService)
 
 module.exports = itemServiceRoutes
