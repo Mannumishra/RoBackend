0ffe57d1303109f639fa11836f67509ee841086d
mannumishra <mannumishra2207@outlook.com>
2024-09-28 17:21:09 +0530

    add chnages

--- Middleware/Multer.js
+++ Middleware/Multer.js
@@ -1,13 +1,18 @@
 const multer = require('multer')
+const fs = require("fs")
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, './public')
+        const dir = "./public"
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir, { recursive: true });
+        }
+        cb(null, dir)
     },
     filename: function (req, file, cb) {
         cb(null, Date.now() + file.originalname)
     }
-    
+
 })
 
 const upload = multer({ storage: storage })
