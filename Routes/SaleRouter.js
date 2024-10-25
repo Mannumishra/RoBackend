1077fa8a963cbb4aaac400cb91f3df7ee279901c
mannumishra <mannumishra2207@outlook.com>
2024-10-25 18:35:28 +0530

    add chnages

--- Routes/SaleRouter.js
+++ Routes/SaleRouter.js
@@ -1,11 +1,11 @@
-const { createSale, getSales, filterSales, getSaleByFEId } = require("../Controller/saleController")
+const { createSale, getSales, filterSales, getSaleByFEId, deleteSale } = require("../Controller/saleController")
 
 const saleRouter = require("express").Router()
 
 
 saleRouter.post("/send-sale", createSale)
 saleRouter.get("/get-sales", getSales)
-
+saleRouter.delete("/delete-sale/:id" ,deleteSale)
 saleRouter.post("/filter-sale-data", filterSales)
 saleRouter.post("/get-sale/fe-id", getSaleByFEId)
 
